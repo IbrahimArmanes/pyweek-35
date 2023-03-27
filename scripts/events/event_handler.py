fbc6f789c760deaa842bb5e8d29cee9680574e11
ahmadaminehamdaoui <90623232+ahmadaminehamdaoui@users.noreply.github.com>
2023-03-27 15:59:09 +0200

    Update event_handler.py

--- scripts/events/event_handler.py
+++ scripts/events/event_handler.py
@@ -1,14 +1,18 @@
 import pygame, sys
 from scripts.events.keydown import handle_keydown
 from scripts.events.keyup import handle_keyup
+from scripts.events.mousedown import handle_mousedown
+from scripts.events.mouseup import handle_mouseup
 
 def shutdown():
     pygame.quit()
     sys.exit()
 
-events_dic = {pygame.QUIT:    shutdown,       # which function is called depending on the event type
-              pygame.KEYDOWN: handle_keydown,
-              pygame.KEYUP:   handle_keyup}
+events_dic = {pygame.QUIT:            shutdown,       # which function is called depending on the event type
+              pygame.KEYDOWN:         handle_keydown,
+              pygame.KEYUP:           handle_keyup,
+              pygame.MOUSEBUTTONDOWN: handle_mousedown,
+              pygame.MOUSEBUTTONUP:   handle_mouseup}
 
 def handle_event(event):                      # call corresponding function
     if event.type in events_dic.keys():
